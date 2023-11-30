fe6010db58a06fa54160603fd86f150f8ff87b60
viktoryia <viktoria.kotejko@gmail.com>
2023-11-30 16:40:05 +0300

    Поиск

--- config/web.php
+++ config/web.php
@@ -56,6 +56,7 @@ $config = [
                 'category/<id:\d+>/page/<page:\d+>' => 'category/view',
                 'product/<id:\d+>' => 'product/view',
                 'product/<id:\d+>' => 'product/view',
+                'search' => 'category/search',
 
             ],
         ],
--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -6,6 +6,7 @@ namespace app\controllers;
 
 use app\models\Category;
 use app\models\Product;
+use yii\base\BaseObject;
 use yii\data\Pagination;
 use yii\web\NotFoundHttpException;
 
@@ -33,6 +34,32 @@ class CategoryController extends AppController
 
         return $this->render('view',compact('products','category', 'pages'));
 
+
+
+    }
+
+    public function actionSearch()
+    {
+        $q = trim(\Yii::$app->request->get('q'));
+        $this->setMeta("Поиск {$q} :: " . \Yii::$app->name);
+        if (!$q)
+        {
+            return $this->render('search');
+        }
+
+        $query= Product::find()->where(['like', 'title', $q]);
+        $pages = new Pagination([
+            'totalCount'=> $query->count(),
+            'pageSize'=>4,
+            'forcePageParam' => false,
+            'pageSizeParam' => false,
+
+        ]);
+        $products = $query->offset($pages->offset)
+            ->limit($pages->limit)
+            ->all();
+
+        return $this->render('search',compact('products', 'pages','q'));
 
 
     }
