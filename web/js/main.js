9d711ec71018a3c21867a179ce8bb0dcd2f40f20
viktoryia <viktoria.kotejko@gmail.com>
2023-12-03 23:07:38 +0300

    Корзина. Часть 1

--- web/js/main.js
+++ web/js/main.js
@@ -65,3 +65,24 @@ paypal.minicart.cart.on('checkout', function (evt) {
         evt.preventDefault();
     }
 });
+
+/* Cart */
+
+$('.add-to-cart').on('click', function () {
+    let id = $(this).data('id');
+    $.ajax({
+        url: 'cart/add',
+        data: {id: id},
+        type: 'GET',
+        success: function (res) {
+            console.log(res);
+        },
+        error: function(){
+            alert('Error!');
+        }
+    });
+    return false;
+});
+
+/* Cart */
+
